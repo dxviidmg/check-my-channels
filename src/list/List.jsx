cafdd02835b75d9b811d5c92539bf1eda0cdf4dc
David <dmg_92@hotmail.com>
2024-02-07 19:05:47 -0600

    v1

--- src/list/List.jsx
+++ src/list/List.jsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { getToken } from '../api/getToken';
 import getChannels from '../api/getChannels';
+import HLSPlayer from '../components/hlsPlayer/HLSPlayer';
 
 const List = () => {
   const [channels, setChannels] = useState([])
-
+  const [channel, setChannel] = useState({"name": "", "link": ""})
+  const [index, setIndex] = useState(0)
 
   const entertainmentStreamingUrl = process.env.REACT_APP_ENTERTAINMENT_STREAMING_URL;
-  const entertainmentStreamingUsername = process.env.REACT_APP_ENTERTAINMENT_STREAMING_USERNAME;
-  const entertainmentStreamingPassword = process.env.REACT_APP_ENTERTAINMENT_STREAMING_PASSWORD;
-
-  const apiUrl = `${entertainmentStreamingUrl}api/channel-list/?is_popular=true/`;
   
   useEffect(() => {
     const fetchData = async () => {
       try {
         const channelsData = await getChannels();
         console.log('channelsData', channelsData)
-//        setChannels(channelsData);
+        setChannels(channelsData);
       } catch (error) {
         console.error('Error fetching channels:', error);
       }
@@ -28,13 +26,24 @@ const List = () => {
   
 
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIndex(index + 1);
+      setChannel(channels[index])
+      if (index == channels.length - 1){
+        console.log('index dentro de cond', index)
+        setIndex(0)
+      }
+    }, 3000);
+  });
+
 
 
 
   return (
     <div>
-      hola
-  {channels}      
+      {JSON.stringify(channel)}
+      {channel?.link !== undefined ? "si": "no"}
     </div>
   )
 }
